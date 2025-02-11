b3fa009a75f5921216ffe84eb9d75d5a402fcc12
Riya Verma <109965632+riya0704@users.noreply.github.com>
2025-02-12 00:48:04 +0530

    refactor: streamline event page layout and enhance search functionality

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,104 +1,67 @@
-import CheckoutButton from '@/components/shared/CheckoutButton';
+import CategoryFilter from '@/components/shared/CategortFilter';
 import Collection from '@/components/shared/Collection';
-import { getEventById, getRelatedEventsByCategory } from '@/lib/action/event.actions';
-import { formatDateTime } from '@/lib/utils';
+import Search from '@/components/shared/Search';
+import { Button } from '@/components/ui/button';
+import { getAllEvents } from '@/lib/action/event.actions';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image';
+import Link from 'next/link';
 
-export default async function EventDetails({
-  params,
-  searchParams,
-}: {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const event = await getEventById(params.id);
+export default async function Home({ searchParams }: SearchParamProps) {
+  // Await searchParams before accessing its properties.
+  const sp = await Promise.resolve(searchParams);
 
-  const relatedEvents = await getRelatedEventsByCategory({
-    categoryId: event.category._id,
-    eventId: event._id,
-    page: searchParams.page as string,
+  const page = Number(sp?.page) || 1;
+  const searchText = (sp?.query as string) || '';
+  const category = (sp?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
   });
 
   return (
     <>
-      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
+            </Button>
+          </div>
+
           <Image 
-            src={event.imageUrl}
-            alt="hero image"
+            src="/assets/images/hero.png"
+            alt="hero"
             width={1000}
             height={1000}
-            className="h-full min-h-[300px] object-cover object-center"
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
-
-          <div className="flex w-full flex-col gap-8 p-5 md:p-10">
-            <div className="flex flex-col gap-6">
-              <h2 className="h2-bold">{event.title}</h2>
-
-              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
-                <div className="flex gap-3">
-                  <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
-                    {event.isFree ? 'FREE' : `$${event.price}`}
-                  </p>
-                  <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
-                    {event.category.name}
-                  </p>
-                </div>
-
-                <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
-                  by{' '}
-                  <span className="text-primary-500">
-                    {event.organizer.firstName} {event.organizer.lastName}
-                  </span>
-                </p>
-              </div>
-            </div>
-
-            <CheckoutButton event={event} />
-
-            <div className="flex flex-col gap-5">
-              <div className="flex gap-2 md:gap-3">
-                <Image src="/assets/icons/calendar.svg" alt="calendar" width={32} height={32} />
-                <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
-                  <p>
-                    {formatDateTime(event.startDateTime).dateOnly} -{' '}
-                    {formatDateTime(event.startDateTime).timeOnly}
-                  </p>
-                  <p>
-                    {formatDateTime(event.endDateTime).dateOnly} -{' '}
-                    {formatDateTime(event.endDateTime).timeOnly}
-                  </p>
-                </div>
-              </div>
-
-              <div className="p-regular-20 flex items-center gap-3">
-                <Image src="/assets/icons/location.svg" alt="location" width={32} height={32} />
-                <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
-              <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
-              <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
-                {event.url}
-              </p>
-            </div>
-          </div>
         </div>
-      </section>
+      </section> 
 
-      {/* EVENTS with the same category */}
-      <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Related Events</h2>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div>
+
         <Collection 
-          data={relatedEvents?.data}
+          data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
-          limit={3}
-          page={searchParams.page as string}
-          totalPages={relatedEvents?.totalPages}
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
         />
       </section>
     </>
